43251411fca08d64becfbccf87e0f8701d02fbcf
Heet-Jain03 <162002913+Heet-Jain03@users.noreply.github.com>
2024-10-26 22:54:34 +0530

    auth.js

--- auth.js
+++ auth.js
@@ -4,9 +4,11 @@ function checkAuth() {
     return !!sessionUser; 
 }
 
+
 function login(username, password) {
     const users = JSON.parse(localStorage.getItem('users')) || {};
 
+   
     if (users[username] && users[username].password === password) {
         sessionStorage.setItem('sessionUser', username); 
         return true; 
@@ -21,11 +23,11 @@ function register(username, email, password) {
     const emailExists = Object.values(users).some(user => user.email === email);
 
     if (usernameExists || emailExists) {
-        return false; 
+        return false; r
     }
 
     users[username] = { email: email, password: password };
-    localStorage.setItem('users', JSON.stringify(users));
+    localStorage.setItem('users', JSON.stringify(users)); 
     return true; 
 }
 
@@ -36,6 +38,6 @@ function logout() {
 
 if (window.location.pathname.endsWith('home.html')) {
     if (!checkAuth()) {
-        window.location.href = 'index.html'; 
+        window.location.href = 'index.html';  
     }
 }
